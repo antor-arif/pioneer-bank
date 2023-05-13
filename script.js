a8d3b9b101575721bf74b08ac1d36de0c04fe02e
antor-arif <113544876+antor-arif@users.noreply.github.com>
2023-05-13 21:10:25 +0600

    last commit

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
 var loginBtn = document.getElementById('login-btn');
 loginBtn.addEventListener('click', function(){
-    var loginArea = document.getElementById('login-area');
+    const loginArea = document.getElementById('login-area');
      loginArea.style.display = "none";
+     const transarea = document.getElementById('transactions-area');
+     transarea.style.display = "block";
+})
+
+const depositBtn = document.getElementById("depositbutton");
+depositBtn.addEventListener('click', function(){
+    const depositMoney = document.getElementById('depositInput').value;
+    const depositAmount = parseFloat(depositMoney);
+    const currentDeposit = document.getElementById('depositAmount').innerText;
+    const newDeposit = parseFloat(currentDeposit);
+    const totalDeposit = depositAmount + newDeposit;
+
+    document.getElementById('depositAmount').innerText = totalDeposit;
+
+
+    const currentBalance = document.getElementById('balanceAmount').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositAmount + currentBalanceNumber;  
+    document.getElementById('balanceAmount').innerText = totalBalance;
+
+    document.getElementById('depositInput').value = "";
+})
+
+const withdrawBtn = document.getElementById('withdrawbutton');
+withdrawBtn.addEventListener('click', function(){
+    const withdrawMoney = document.getElementById('withdrawInput').value;
+    const withdrawAmount = parseFloat(withdrawMoney);
+    const newWithdraw = document.getElementById('withdrawAmount').innerText;
+    const newWthd = parseFloat(newWithdraw);
+    const totalWithdraw = withdrawAmount + newWthd;
+    document.getElementById('withdrawAmount').innerText = totalWithdraw;
+
+    const currentBalance = document.getElementById('balanceAmount').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = currentBalanceNumber - totalWithdraw;  
+    document.getElementById('balanceAmount').innerText = totalBalance;
+    document.getElementById('withdrawInput').innerText = "";
+
+
 })
